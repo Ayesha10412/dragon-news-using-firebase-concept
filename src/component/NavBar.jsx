e391a251f1fc21aea611270d01bfc7877f92b6f4
Your Name <k.bristi057@gmail.com>
2024-11-16 11:12:24 +0600

    added the nav

---
+++ src/component/NavBar.jsx
@@ -0,0 +1,25 @@
+import user from '../assets/user.png'
+import { Link } from 'react-router-dom';
+
+const NavBar = () => {
+    return (
+        <div className='flex justify-between mt-10'>
+            
+<div></div>
+<div className="nav space-x-5">
+    <Link to='/' >Home</Link>
+    <Link to='/career'  >Career</Link>
+    <Link to='/about' >About</Link>
+
+</div>
+
+<div className="login flex items-center gap-2">
+    <img src={user} alt="" />
+    <button className= 'bg-[#403F3F] text-white rounded-none btn px-4'>Login</button>
+</div>
+
+        </div>
+    );
+};
+
+export default NavBar;
--- src/layouts/HomeLayout.jsx
+++ src/layouts/HomeLayout.jsx
@@ -1,17 +1,28 @@
 import Header from "../component/Header";
 import LatestNews from "../component/LatestNews";
+import NavBar from "../component/NavBar";
 
 const HomeLayout = () => {
     return (
-        <div>
+        <div className="font-poppins">
 <header>
     <Header></Header>
     <section className="w-11/12 mx-auto">
         <LatestNews></LatestNews>
     </section>
 </header>
-<nav></nav>
-<main></main>
+
+<nav className="w-11/12 mx-auto py-2">
+<NavBar></NavBar>
+</nav>
+
+<main  className="w-11/12 mx-auto pt-5 grid grid-cols-12 gap-3 " >
+
+<aside className="left col-span-3">Left Navbar</aside>
+<section className=" col-span-6">Main content</section>
+<aside className=" col-span-3">RIght navbar</aside>
+
+</main>
 
 
         </div>
