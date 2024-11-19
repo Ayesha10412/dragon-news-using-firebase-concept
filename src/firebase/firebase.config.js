6734d10df825ce36c6f0cc081ca9de71c36048c2
Your Name <k.bristi057@gmail.com>
2024-11-19 06:14:47 +0600

    added the secure way to add key

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -6,12 +6,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyB8HZAWIv0Ga0gDIGXJgSue1Fa66mnuQ7c",
-  authDomain: "dragon-news-e0dca.firebaseapp.com",
-  projectId: "dragon-news-e0dca",
-  storageBucket: "dragon-news-e0dca.firebasestorage.app",
-  messagingSenderId: "354977902183",
-  appId: "1:354977902183:web:208fbf1d53d8e50d21aec2"
+  apiKey:import.meta.env.VITE_apiKey, 
+  authDomain:import.meta.env.VITE_authDomain,
+  projectId:import.meta.env.VITE_projectId,
+  storageBucket:import.meta.env.VITE_storageBucket,
+  messagingSenderId:import.meta.env.VITE_messagingSenderId, 
+  appId:import.meta.env.VITE_appId, 
 };
 
 // Initialize Firebase
